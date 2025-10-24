7550aa08ace2f9a6ef624f3b4c11620258fb4d36
Daniel Botnik <danielbotnik@gmail.com>
2025-10-24 11:13:01 +0300

    tests(Engine): added sanity test for loops

---
+++ tests/engine/test_loops.py
@@ -0,0 +1,25 @@
+from binary_function import BinaryFunction, Loop
+from pcode_engine import Engine
+from project import Project
+from engine_types import Arg, BinaryOp, MemoryAccess, MemoryAccessType, Register
+
+
+class TestLoops:
+    def test_loop_sanity(self):
+        CODE = b"<\x02\x00^$BY\xd4\x8cC\x00\x00P`\x00\x06\x00\x00\x10!\x8cC\x00\x04\x10d\x00\x03\x00\x00\x00\x00\x08\x10\xfeM$B\x00$\x03\xe0\x00\x08\x00\x00\x00\x00"
+        ADDR = 0x0043F92C
+
+        project = Project("MIPS:BE:32:default")
+        bin_func = BinaryFunction(ADDR, CODE, project)
+        engine = Engine(bin_func)
+
+        base = Register(8, 0x43F934, bin_func)
+        expected_loop = Loop(
+            start=0x43F934,
+            blocks={0x0043F934, 0x0043F940, 0x0043F94C},
+            exit_conditions={
+                0x43F938: BinaryOp(MemoryAccess(0x43F934, base, 0x0, MemoryAccessType.LOAD), 0x0, "=="),
+                0x43F944: BinaryOp(MemoryAccess(0x43F934, base, 0x4, MemoryAccessType.LOAD), Arg(0), "=="),
+            },
+        )
+        assert bin_func.loops_dict_start_address[0x43F934] == expected_loop
