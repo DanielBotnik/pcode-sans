25f6b95a6bfe44b9a305d07c51aa034a49c263f4
Daniel Botnik <danielbotnik@gmail.com>
2025-11-01 11:01:32 +0200

    tests: added test_callsite_with_many_stack_arguments

--- tests/engine/test_callsites.py
+++ tests/engine/test_callsites.py
@@ -55,3 +55,31 @@ class TestCallSites:
             args=frozendict({0: Arg(0), 1: Arg(1), 2: PKCS7_it}),
         )
         assert expected_callsite in engine.callsites
+
+    def test_callsite_with_many_stack_arguments(self):
+        CODE = b"<\x1c\x00_\x00\xa08!'\x9c'\xe0<\x05\x00['\xbd\xff\xc8\x00\x800!\x8f\x99\x99l\x8f\x84\x99\xb0$\xa5\x91\x18\xaf\xbf\x004\xaf\xbc\x00(\xaf\xa0\x00\x10\xaf\xa0\x00\x14\xaf\xa0\x00\x18\xaf\xa0\x00\x1c\x03 \xf8\t\xaf\xa0\x00 \x8f\xbf\x004\x03\xe0\x00\x08'\xbd\x008"
+        ADDR = 0x004A0390
+
+        project = Project("MIPS:BE:32:default")
+        bin_func = BinaryFunction(ADDR, CODE, project)
+        engine = Engine(bin_func)
+
+        assert len(engine.callsites) == 1
+        expected_callsite = CallSite(
+            addr=0x004A03CC,
+            target=UnaryOp(0x005EC14C, "*"),
+            args=frozendict(
+                {
+                    0: UnaryOp(0x005EC190, "*"),
+                    1: 0x005A9118,
+                    2: Arg(0),
+                    3: Arg(1),
+                    4: 0,
+                    5: 0,
+                    6: 0,
+                    7: 0,
+                    8: 0,
+                }
+            ),
+        )
+        assert expected_callsite in engine.callsites
